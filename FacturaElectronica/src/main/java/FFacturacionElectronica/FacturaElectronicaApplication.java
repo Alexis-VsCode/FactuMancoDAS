817f31f17eddde7e431d50e555703afc4ee23471
Alexis-VsCode <68306817+Alexis-VsCode@users.noreply.github.com>
2022-06-20 12:14:32 -0500

    PROYECTO INCIAL
    los arhcivos bas epar a el proyecto inicial

---
+++ FacturaElectronica/src/main/java/FFacturacionElectronica/FacturaElectronicaApplication.java
@@ -0,0 +1,19 @@
+package FFacturacionElectronica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacturaElectronicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacturaElectronicaApplication.class, args);
+	}
+//Cambio  Aplicacion Principal
+//HOLA MUNDO 
+	//CAMBIO 
+	//CAMBIO
+	//CAMBIO NUEVO 
+	//CAMBIO 
+	//ANDREAA
+}
